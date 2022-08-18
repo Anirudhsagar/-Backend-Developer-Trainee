479b13789ec8bdeaf33c25100886ab1094e74786
anirudh sagar <you@example.com>
2022-08-18 13:51:03 +0530

    session 3 assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://anirudhsagar:fgAGHtahZoVNyIR3@cluster0.btvli.mongodb.net/Anirudhbookses3-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
